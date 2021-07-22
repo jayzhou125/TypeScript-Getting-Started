3c3e2d76fd93e5c5dd0d536208c223651c9787ed
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-07-22 10:04:37 -0400

    update multimath game to be able to get userinput and display the name accordingly

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,30 @@
 function startGame(){
     // starting a new game
-    let playerName: string = 'Audrey';
+    let playerName: string | undefined = getInput('playername');
     logPlayer(playerName);
 
-    var messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(80, playerName);
+    //var messagesElement = document.getElementById('messages');
+    //messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer(name: string){
+function logPlayer(name: string = 'MultiMath Player'): void{
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInput(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    if (inputElement.value === ''){
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void{
+    const scoreElement : HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
